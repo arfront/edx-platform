b1288d0c5a9dab496c49e7c0d1c11ae4e4bd3778
wrydeveloper <13047908816@163.com>
2019-11-19 15:48:32 +0800

    [ADD]Adjust code style,Added delete leave people func

--- cms/djangoapps/api/urls.py
+++ cms/djangoapps/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from cms.djangoapps.api.v1.views.kh_user_manage import GetuserfromDingTalkView
+from cms.djangoapps.api.v1.views.kh_user_manage import GetuserfromDingTalkView, RemoveleavejobuserView
 
 urlpatterns = [
     url(r'^v1/', include('cms.djangoapps.api.v1.urls', namespace='v1')),
     url(r'^v1/kh_insert_dingtalk_user/', GetuserfromDingTalkView.as_view()),
+    url(r'^v1/kh_remove_leave_job_user/', RemoveleavejobuserView.as_view()),
 ]
--- cms/static/js/maintenance/index.js
+++ cms/static/js/maintenance/index.js
@@ -10,7 +10,8 @@ define(['jquery'], function ($) {
                 type: "GET",
                 success: function (result) {
                   if (result['status'] == '10001') {
-                    $('.is-shown').css('display', 'block')
+                    $('.is-shown').css('display', 'block');
+                    $('.result-is-shown').css('display', 'none');
                     var success_user_info = result['result']['success_user_info'];
                     var fail_user_info = result['result']['fail_user_info'];
                     if (success_user_info.length > 0) {
@@ -32,6 +33,31 @@ define(['jquery'], function ($) {
                 }
 
             })
-        })
+        });
+
+        $('.dingtalk-button-delete').click(function (event) {
+            $(".dingtalk-delete-user-detail").html('');
+            var host=window.location.host;
+            var protocolStr = document.location.protocol;
+            $.ajax({
+              url:  protocolStr+ '//' + host + "/api/v1/kh_remove_leave_job_user/",
+              type: "GET",
+              success: function (result) {
+                $('.is-shown').css('display', 'none');
+                $('.result-is-shown').css('display', 'block');
+                $('#dingtalk-delete-msg').text(result['msg']);
+                if (result['detail'].length > 0) {
+                  $.each(result['detail'], function (index, item) {
+                    var real_name = item['real_name']
+                    var username = item['username']
+                    var email = item['email']
+                    $('.dingtalk-delete-user-detail').html($('.dingtalk-delete-user-detail').html() +
+                                                              "<li>" + "&nbsp;&nbsp;&nbsp;&nbsp;账号:&nbsp;" + username + "&nbsp;&nbsp;真实姓名:&nbsp;" + real_name
+                                                                        + "&nbsp;&nbsp;邮箱:&nbsp;" + email + '</li>')
+                  })
+                }
+              }
+            })
+        });
     }
 })
