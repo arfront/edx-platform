e3b10beec6406453e26fdb2d0bb359d6ef853db9
wrydeveloper <13047908816@163.com>
2020-04-17 10:38:44 +0800

    [fix]fix migrate error caused by merge

--- common/djangoapps/arfrontconfig/migrations/0006_alipayinfoconfig.py
+++ common/djangoapps/arfrontconfig/migrations/0006_alipayinfoconfig.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('arfrontconfig', '0004_weihouaccountconfig'),
+        ('arfrontconfig', '0005_newerguiderecord'),
     ]
 
     operations = [
