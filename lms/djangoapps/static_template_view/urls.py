30d25d645c444d2527ce78aeb6d44c24e3259ae2
dvkyle <180725842@qq.com>
2020-07-22 11:40:19 +0800

    [ADD]Added func of pdf xblock

--- lms/djangoapps/static_template_view/urls.py
+++ lms/djangoapps/static_template_view/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     url(r'^404$', views.render_404, name='static_template_view.views.render_404'),
     url(r'^500$', views.render_500, name='static_template_view.views.render_500'),
 
+    url(r'^about$', views.render, {'template': 'about.html'}, name="about"),
+
     url(r'^blog$', views.render, {'template': 'blog.html'}, name="blog"),
     url(r'^contact$', views.render, {'template': 'contact.html'}, name="contact"),
     url(r'^donate$', views.render, {'template': 'donate.html'}, name="donate"),
